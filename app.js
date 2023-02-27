0f95d6020661ea29d6351f82cf1b32dd7e26f9e3
Del Barnwell <dbarnwel@google.com>
2023-02-27 10:53:48 -0500

    initial check-in to get things working

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
-require('dotenv').config();
-const Express = require('express');
-const app = Express();
-const port = process.env.PORT || 3004;
+
+const Express = require('express')
+
+require('dotenv').config()
+//console.log(process.env)
+
+const app = Express()
+const port = process.env.PORT || 3004
 
 
 const MongoManager = require('./src/shared/db/mongodb/mongo-manager')
-const MiddleWare = require('./src/shared/middleware/base-middleware');
-const HealthRoutes = require('./src/routes/health.routes');
-const AdminRoutes = require('./src/routes/admin.routes');
-const PublicRoutes = require('./src/routes/public.routes');
+const MiddleWare = require('./src/shared/middleware/base-middleware')
+const HealthRoutes = require('./src/routes/health.routes')
+const AdminRoutes = require('./src/routes/admin.routes')
+const PublicRoutes = require('./src/routes/public.routes')
 const AgentRoutes = require('./src/routes/agent.routes')
 const regionRouter = require('./src/routes/region.routes')
 
@@ -18,14 +22,14 @@ app.use(Express.json())
 
 
 MiddleWare.registerBaseMiddleWare(app)
-HealthRoutes.registerHealthRoutes(app);
-AdminRoutes.registerAdminRoutes(app);
-PublicRoutes.registerPublicRoutes(app);
+HealthRoutes.registerHealthRoutes(app)
+AdminRoutes.registerAdminRoutes(app)
+PublicRoutes.registerPublicRoutes(app)
 AgentRoutes.registerAgentRoutes(app)
 regionRouter.registerRegionRoutes(app)
 
 
-MongoManager.openMongoConnection();
+MongoManager.openMongoConnection(process.env.MONGO_URI)
 app.listen(port, () => {
     console.log(`server is listening on port ${port}`)
 })
--- src/shared/db/mongodb/mongo-manager.js
+++ src/shared/db/mongodb/mongo-manager.js
@@ -1,16 +1,16 @@
 const mongoose = require('mongoose')
-require('dotenv').config()
 
 
-const openMongoConnection = () => {
-    const db = mongoose.connection;
-    db.on('error', console.error.bind(console, 'connection error:'));
-    db.once('open', function callback () {
-        console.log("connected to MongoDB");
-    });
-    mongoose.connect(process.env.MONGO_URI);
-};
+const openMongoConnection = (uri) => {
+    const db = mongoose.connection
+    db.on('error', console.error.bind(console, 'connection error:'))
+    db.once('open', () => {
+        console.log("connected to MongoDB")
+    })
+    mongoose.connect(uri)
+    console.log(`uri: ${uri}`)
+}
 
-mongoose.set('strictQuery', true) 
+mongoose.set('strictQuery', true)
 
-module.exports = {openMongoConnection};
+module.exports = { openMongoConnection }
